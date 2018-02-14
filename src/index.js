f843af150ebe441a4b0509426a0549d95f7dddb3
Nasim Akhtar <nasimakhtar1996@gmail.com>
2018-02-14 14:27:44 +0530

    added Landing Page

--- src/index.js
+++ src/index.js
@@ -1,51 +1,74 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-
+import { Col, Row, Grid } from 'react-native-easy-grid';
 import { Button, Header } from '../storybook/elements';
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topContainer: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingTop: 20,
+    paddingRight: 20,
+  },
+  textStyle: {
+    backgroundColor: 'transparent',
+    color: '#ffff',
+    fontSize: 16,
+  },
+  backgroundImage: {
     flex: 1,
+    position: 'absolute',
+  },
+  logo: {
+    width: 165,
+    height: 114,
+  },
+  justifyContent: {
+    justifyContent: 'center',
   },
 });
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'powderblue',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Text>hello</Text>
-        </View>
-        <View
-          style={{
-            flex: 2,
-            backgroundColor: 'skyblue',
-
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Text>hello</Text>
-        </View>
-        <View
-          style={{
-            flex: 3,
-            backgroundColor: 'steelblue',
-
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Text>hello</Text>
-        </View>
-      </View>
+      <Grid>
+        <Image
+          source={require('./img/wb.png')}
+          style={styles.backgroundImage}
+        />
+        <Row>
+          <Grid>
+            <Row size={1} style={styles.topContainer}>
+              <Text style={[styles.textStyle, { fontWeight: 'bold' }]}>
+                Login
+              </Text>
+            </Row>
+            <Row size={2} style={styles.container}>
+              <Image source={require('./img/logo.png')} style={styles.logo} />
+            </Row>
+            <Row size={1} style={styles.justifyContent}>
+              <Text style={styles.textStyle}>Welcome To White & Bold CO.</Text>
+            </Row>
+          </Grid>
+        </Row>
+        <Row>
+          <Grid>
+            <Row size={1} style={styles.container}>
+              <Button text="facebook" />
+            </Row>
+            <Row size={1} style={styles.container}>
+              <Button text="facebook" />
+            </Row>
+            <Row size={2} style={styles.justifyContent}>
+              <Text style={styles.textStyle}>More Option</Text>
+            </Row>
+          </Grid>
+        </Row>
+      </Grid>
     );
   }
 }
